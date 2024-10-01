8fa994b551fb65fb4a101b611a0014839e9d9f36
KassKerbal <76430636+KassKerbal@users.noreply.github.com>
2024-10-01 15:57:28 +0200

    add xlsx
    se añade la librería xlsx y se añade el conversor a csv

--- at-csv-maker/src/App.js
+++ at-csv-maker/src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
+import ExcelUploader from './components/excelUploader.jsx';
 
 function App() {
-
-  const irene = "Irene";
 
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>{irene}</p>
-        <input onChange={(e) => console.log(e.target.value)}></input>
-        <label>Carga tu EXCEL Aquí</label>
+        <ExcelUploader />
+
       </header>
     </div>
   );
---
+++ at-csv-maker/src/components/excelUploader.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import * as XLSX from "xlsx";
+
+function ExcelUploader() {
+  const [csvData, setCsvData] = useState("");
+  const [blob, setBlob] = useState("");
+
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const data = new Uint8Array(e.target.result);
+      const workbook = XLSX.read(data, { type: "array" });
+      const firstSheet = workbook.Sheets[workbook.SheetNames[0]]; // primera hoja
+      const csv = XLSX.utils.sheet_to_csv(firstSheet);
+
+      setCsvData(csv);
+
+      const newBlob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
+      setBlob(URL.createObjectURL(newBlob))
+    };
+
+    reader.readAsArrayBuffer(file);
+  };
+
+  return (
+    <>
+      <input
+        type="file"
+        accept=".xlsx, .xls"
+        onChange={handleFileUpload}
+      ></input>
+      <label>Carga tu EXCEL Aquí</label>
+     {csvData && <p>CSV Generado: <a href={blob} download="archivo.csv" id="downloadLink">Descarga aquí</a></p>}
+      {csvData && (
+                <div>
+                    <h2>CSV Generado:</h2>
+                    <textarea rows="30" cols="200" value={csvData} readOnly></textarea>
+                </div>
+            )}
+
+    </>
+  );
+}
+
+export default ExcelUploader;
